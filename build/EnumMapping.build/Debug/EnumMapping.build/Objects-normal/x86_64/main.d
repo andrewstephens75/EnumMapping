55622c175015e369a1a9eb9ce25c4ef73059ba4b
Andrew Stephens <andrew.stephens75@gmail.com>
2018-05-07 08:31:19 -0400

    Removed binaries, that will teach me to push repos while travelling

--- build/EnumMapping.build/Debug/EnumMapping.build/Objects-normal/x86_64/main.d
+++
@@ -1,3 +0,0 @@
-dependencies: /Users/andrew/Source/randomstuff/stringconv/src/main.cpp \
-  /Users/andrew/Source/randomstuff/stringconv/src/enum_mapping.h \
-  /Users/andrew/Source/randomstuff/stringconv/src/dumb_test_helpers.h
--- build/stringconv.build/Debug/stringconv.build/Objects-normal/x86_64/main.d
+++
@@ -1,4 +0,0 @@
-dependencies: \
-  /Users/andrew/Source/randomstuff/stringconv/stringconv/main.cpp \
-  /Users/andrew/Source/randomstuff/stringconv/stringconv/enum_mapping.h \
-  /Users/andrew/Source/randomstuff/stringconv/stringconv/dumb_test_helpers.h
